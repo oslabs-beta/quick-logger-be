4a5775382db2a0816e6677083a8789e3c840cb39
Crystal Pederson <crystalpederson88@gmail.com>
2022-04-07 19:10:34 -0700

    add middleware

---
+++ sherlogs-backend-logger.js
@@ -0,0 +1,41 @@
+const morgan = require('morgan');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf, json } = format;
+const HttpStreamTransport = require('winston-transport-http-stream');
+require('dotenv').config();
+
+const loggerBe = () => {
+    const myFormat = printf(({ level, message, label }) => {
+        const logInfo = {
+          level,
+          message: `${message}`,
+          label: `${label}`,
+        };
+        return JSON.stringify(logInfo);
+      });
+      
+      const logger = createLogger({
+        level: 'debug',
+        format: combine(myFormat, timestamp(), json()),
+        transports: [
+          new transports.Console({ level: 'info' }),
+          new transports.File({
+            filename: 'app.log',
+            level: 'debug',
+          }),
+          new HttpStreamTransport({
+            url: process.env.LOGS_API_ENDPOINT
+          })
+        ],
+      });
+      
+      const myStream = {
+        write: (text) => {
+          logger.info(text);
+        },
+      };
+      
+    morgan('combined', { stream: myStream })
+}
+
+module.exports.loggerBe
